a9044d5bbd9c3407386e1bf910d7771f370b7ae8
Oluwafemi Adadayo <hallengray@gmail.com>
2026-04-12 09:43:23 +0100

    chore(core): update security module exports

--- packages/core/src/rag_forge_core/security/__init__.py
+++ packages/core/src/rag_forge_core/security/__init__.py
@@ -1,1 +1,50 @@
 """Security module: InputGuard and OutputGuard pipelines."""
+
+from rag_forge_core.security.citations import CitationValidationResult, CitationValidator
+from rag_forge_core.security.faithfulness import FaithfulnessChecker, FaithfulnessResult
+from rag_forge_core.security.injection import (
+    InjectionCheckResult,
+    PromptInjectionClassifier,
+    PromptInjectionDetector,
+)
+from rag_forge_core.security.input_guard import GuardResult, InputGuard
+from rag_forge_core.security.output_guard import OutputGuard, OutputGuardResult
+from rag_forge_core.security.pii import (
+    PIIDetection,
+    PIIScannerProtocol,
+    PIIScanResult,
+    PresidioPIIScanner,
+    RegexPIIScanner,
+)
+from rag_forge_core.security.rate_limiter import (
+    InMemoryRateLimitStore,
+    RateLimiter,
+    RateLimitResult,
+    RateLimitStore,
+)
+from rag_forge_core.security.staleness import StalenessChecker, StalenessResult
+
+__all__ = [
+    "CitationValidationResult",
+    "CitationValidator",
+    "FaithfulnessChecker",
+    "FaithfulnessResult",
+    "GuardResult",
+    "InMemoryRateLimitStore",
+    "InjectionCheckResult",
+    "InputGuard",
+    "OutputGuard",
+    "OutputGuardResult",
+    "PIIDetection",
+    "PIIScannerProtocol",
+    "PIIScanResult",
+    "PresidioPIIScanner",
+    "PromptInjectionClassifier",
+    "PromptInjectionDetector",
+    "RateLimiter",
+    "RateLimitResult",
+    "RateLimitStore",
+    "RegexPIIScanner",
+    "StalenessChecker",
+    "StalenessResult",
+]
